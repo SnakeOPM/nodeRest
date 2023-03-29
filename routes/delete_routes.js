b1fe1c38bfbc08e4c0395aab87366b38aa28d852
SnakeOPM <snikersdaun@gmail.com>
2023-03-30 01:05:07 +1000

    renew docs

--- routes/delete_routes.js
+++ routes/delete_routes.js
@@ -7,13 +7,24 @@ const delete_controllers = require("../controllers/delete_controller");
  * /delete/post:
  *   delete:
  *     summary: delete a post
- *     description: deteletes a post by id
+ *     description: deteletes a post by id req delID: int
  *     produces:
  *       - application/json
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: integer
+ *                 example: 1
  *     responses:
  *       200:
  *         description: OK
  */
+
 router.delete("/post", delete_controllers.deletePost);
 
 module.exports = router;
--- routes/post_routes.js
+++ routes/post_routes.js
@@ -6,10 +6,24 @@ const post_controllers = require("../controllers/post_controller");
  * @swagger
  * /post/new_user:
  *   post:
- *     summary: creates new user
- *     description: creates new user with username
+ *     summary: Creates a new user
+ *     description: Creates a new user with a username and summary req newUser and newSummary(optional) in request body
  *     produces:
- *       - application/json
+ *       application/json
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               newUser:
+ *                 type: string
+ *               newSummary:
+ *                 type: string
+ *           example:
+ *             newUser: john_doe
+ *             newSummary: i'm guy
  *     responses:
  *       200:
  *         description: OK
@@ -21,9 +35,26 @@ router.post("/new_user", post_controllers.add_user);
  * /post/new_post:
  *   post:
  *     summary: creates new post
- *     description: creates new post, you need to provide an id of user that belongs to this post
+ *     description: creates new post, you need to provide an id of user that belongs to this post requires newTitle, newBody, userPostId: int
  *     produces:
  *       - application/json
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               newTitle:
+ *                 type: string
+ *               newBody:
+ *                 type: string
+ *               userPostId:
+ *                 type: string
+ *             example:
+ *               newTitle: aba
+ *               newBody: fgdsfgds
+ *               userPostId: 27
  *     responses:
  *       200:
  *         description: OK
